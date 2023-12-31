97b466b7c950d6fa49f09e1b880987f8ed51e889
ShafiSQAE <shafi.sqa@gmail.com>
2023-12-31 14:56:09 +0800

    Loop

---
+++ src/BasicProgramming/ForLoop.java
@@ -0,0 +1,12 @@
+package BasicProgramming;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        System.out.println("Basic for loop to print a string 10 times");
+        System.out.println("==========================================");
+        for(int i=0; i<10; i++){
+            System.out.println("Hello World");
+        }
+    }
+}
